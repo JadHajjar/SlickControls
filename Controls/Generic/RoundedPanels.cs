e077d08bf66e3101dc812856e6fc914e8abb7b67
JadHajjar <jad@brains-lb.com>
2023-03-08 16:22:28 +0200

    Rounded Panels

---
+++ Controls/Generic/RoundedPanels.cs
@@ -0,0 +1,43 @@
+﻿using Extensions;
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SlickControls
+{
+	public class RoundedPanel : DBPanel
+	{
+		protected override void OnPaintBackground(PaintEventArgs e)
+		{
+			e.Graphics.Clear(Parent?.BackColor ?? BackColor);
+
+			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+			using (var brush = new SolidBrush(BackColor))
+				e.Graphics.FillRoundedRectangle(brush, ClientRectangle.Pad(0, 0, 1, 1), Padding.Left);
+		}
+	}
+
+	public class RoundedTableLayoutPanel : DBTableLayoutPanel
+	{
+		protected override void OnPaintBackground(PaintEventArgs e)
+		{
+			e.Graphics.Clear(Parent?.BackColor ?? BackColor);
+
+			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+			using (var brush = new SolidBrush(BackColor))
+				e.Graphics.FillRoundedRectangle(brush, ClientRectangle.Pad(0, 0, 1, 1), Padding.Left);
+		}
+	}
+
+	public class RoundedFlowLayoutPanel : DBFlowLayoutPanel
+	{
+		protected override void OnPaintBackground(PaintEventArgs e)
+		{
+			e.Graphics.Clear(Parent?.BackColor ?? BackColor);
+
+			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+			using (var brush = new SolidBrush(BackColor))
+				e.Graphics.FillRoundedRectangle(brush, ClientRectangle.Pad(0, 0, 1, 1), Padding.Left);
+		}
+	}
+}
