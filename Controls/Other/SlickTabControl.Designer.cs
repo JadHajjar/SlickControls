6e7863fde5580a565fc19c16532ea5a041d4239d
JadHajjar <jad@brains-lb.com>
2023-08-01 17:16:38 +0300

    Update SlickTabControl.Designer.cs

--- Controls/Other/SlickTabControl.Designer.cs
+++ Controls/Other/SlickTabControl.Designer.cs
@@ -31,6 +31,7 @@
 			this.P_Tabs = new System.Windows.Forms.Panel();
 			this.P_Content = new SlickControls.DBPanel();
 			this.ScrollBar = new SlickControls.SlickScroll();
+			this.slickSpacer1 = new SlickControls.SlickSpacer();
 			this.P_Content.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -48,9 +49,9 @@
 			// 
 			this.P_Content.Controls.Add(this.ScrollBar);
 			this.P_Content.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.P_Content.Location = new System.Drawing.Point(0, 30);
+			this.P_Content.Location = new System.Drawing.Point(0, 31);
 			this.P_Content.Name = "P_Content";
-			this.P_Content.Size = new System.Drawing.Size(612, 319);
+			this.P_Content.Size = new System.Drawing.Size(612, 318);
 			this.P_Content.TabIndex = 1;
 			this.P_Content.Paint += new System.Windows.Forms.PaintEventHandler(this.P_Content_Paint);
 			// 
@@ -59,19 +60,30 @@
 			this.ScrollBar.AutoSizeSource = true;
 			this.ScrollBar.Dock = System.Windows.Forms.DockStyle.Right;
 			this.ScrollBar.LinkedControl = null;
-			this.ScrollBar.Location = new System.Drawing.Point(606, 0);
+			this.ScrollBar.Location = new System.Drawing.Point(604, 0);
 			this.ScrollBar.Name = "ScrollBar";
-			this.ScrollBar.Size = new System.Drawing.Size(6, 319);
+			this.ScrollBar.Size = new System.Drawing.Size(8, 318);
 			this.ScrollBar.SmallHandle = true;
 			this.ScrollBar.Style = SlickControls.StyleType.Vertical;
 			this.ScrollBar.TabIndex = 0;
 			this.ScrollBar.TabStop = false;
 			this.ScrollBar.Text = "ScrollBar";
 			// 
+			// slickSpacer1
+			// 
+			this.slickSpacer1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.slickSpacer1.Location = new System.Drawing.Point(0, 30);
+			this.slickSpacer1.Name = "slickSpacer1";
+			this.slickSpacer1.Size = new System.Drawing.Size(612, 1);
+			this.slickSpacer1.TabIndex = 2;
+			this.slickSpacer1.TabStop = false;
+			this.slickSpacer1.Text = "slickSpacer1";
+			// 
 			// SlickTabControl
 			// 
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
 			this.Controls.Add(this.P_Content);
+			this.Controls.Add(this.slickSpacer1);
 			this.Controls.Add(this.P_Tabs);
 			this.Name = "SlickTabControl";
 			this.Size = new System.Drawing.Size(612, 349);
@@ -86,5 +98,6 @@
 		private System.Windows.Forms.Panel P_Tabs;
 		private DBPanel P_Content;
 		public SlickScroll ScrollBar;
+		private SlickSpacer slickSpacer1;
 	}
 }
